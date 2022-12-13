3ac8029d44414cd43b45c077d317593d9bf2d5a1
MichaelTokarev <michael.voskanyan@icloud.com>
2022-12-12 22:04:13 -0700

    Added new parameters to guns for automatic/semiautomatic, plus fire rate/fire cooldown and bullets per shot(shotguns shoot multiple projectiles)

--- Scenes/Scripts/Gun.gd
+++ Scenes/Scripts/Gun.gd
@@ -6,12 +6,15 @@ extends Spatial
 
 #Bullet Data
 var bulletList = []
-export var bulletSpeed = 50
+export var bulletSpeed = 100.0
 export var bulletDistance = 1000
-export var gunLength = 1
-export var gunHeight = 1
 export var decay = 0.9
-var bulletScene = load('res://Scenes/Prefabs/Bullet.tscn') 
+export var bulletTimeAlive = 2.0
+export var bulletPerShot = 1
+export var reloading = 0.25
+export var reloadTime = 1.0
+export var automatic = 1
+var bulletScene = load('res://Scenes/Prefabs/Bullet.tscn')
 
 onready var map = $"../.."
 # Called when the node enters the scene tree for the first time.
@@ -21,14 +24,36 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var this = $"."
-	if Input.is_action_pressed("shoot"):
+	if automatic == 1:
+		if Input.is_action_pressed("shoot"):
+			auto_fire(this)
+			reloading -= delta
+	elif automatic == 0:
+		if Input.is_action_just_pressed("shoot"):
+			semi_fire(this)
+			reloading -= delta
+
+func auto_fire(this):
+	if reloading <= 0.0:
+		for i in bulletPerShot:
+			var bullet = bulletScene.instance()
+			var globalRotation = this.get_global_rotation()
+			var rotation = fmod(globalRotation.y, PI*2)
+			#		print("rotation:", rotation)
+			var bulletForceRotated = Vector3(0, 0, -bulletSpeed).rotated(Vector3.UP, rotation)
+			bullet.transform.origin = this.get_global_transform().origin + Vector3(0, 0, 0).rotated(Vector3.UP, rotation)
+			bullet.apply_central_impulse(bulletForceRotated)
+			map.add_child(bullet)
+		reloading = reloadTime
+
+func semi_fire(this):
+	for i in bulletPerShot:
 		var bullet = bulletScene.instance()
 		var globalRotation = this.get_global_rotation()
 		var rotation = fmod(globalRotation.y, PI*2)
-		print("rotation:", rotation)
+	#		print("rotation:", rotation)
 		var bulletForceRotated = Vector3(0, 0, -bulletSpeed).rotated(Vector3.UP, rotation)
-		
-		bullet.transform.origin = this.get_global_transform().origin + Vector3(0, gunHeight, -gunLength).rotated(Vector3.UP, rotation)
-		
+		bullet.transform.origin = this.get_global_transform().origin + Vector3(0, 0, 0).rotated(Vector3.UP, rotation)
 		bullet.apply_central_impulse(bulletForceRotated)
 		map.add_child(bullet)
+	reloading = reloadTime
